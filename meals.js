4b8c1f303fa3e2358edf73498533917ba5a5d63c
sharulislam <sharulislam60@gmail.com>
2023-03-27 22:56:31 +0600

    first commit

---
+++ meals.js
@@ -0,0 +1,61 @@
+const loadMeals = (search) => {
+  const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${search}`;
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => displayMeals(data.meals));
+};
+
+const displayMeals = (meals) => {
+  const mealContainer = document.getElementById("meal-container");
+  mealContainer.innerHTML = ``;
+  meals.forEach((meal) => {
+    console.log(meal);
+    const mealDiv = document.createElement("div");
+    mealDiv.classList.add("col");
+    mealDiv.innerHTML = `
+        <div onclick="loadMealDetail(${meal.idMeal})" class="card">
+                <img src="${meal.strMealThumb}" class="card-img-top" alt="...">
+                <div class="card-body">
+                  <h5 class="card-title">${meal.strMeal}</h5>
+                  <p class="card-text">${meal.strInstructions.slice(0, 200)}</p>
+                </div>
+              </div>
+        
+        `;
+    mealContainer.appendChild(mealDiv);
+  });
+};
+
+const searchFood = () => {
+  const searchField = document.getElementById("search-field");
+  const searchText = searchField.value;
+  loadMeals(searchText);
+  searchField.value = "";
+};
+
+const loadMealDetail = (idMeal) => {
+  // console.log('get details of id ',idMeal);
+  const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${idMeal}`;
+
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => displayMealDetails(data.meals[0]));
+};
+
+const displayMealDetails = (meal) => {
+  const detailContainer = document.getElementById("detail-container");
+  detailContainer.innerHTML=``;
+  const mealDiv = document.createElement("div");
+  mealDiv.classList.add("card");
+  mealDiv.innerHTML = `
+  <img src="${meal.strMealThumb}" class="card-img-top" alt="...">
+              <div class="card-body">
+                <h5 class="card-title">${meal.strMeal}</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <a href="#" class="btn btn-primary">Go somewhere</a>
+              </div>
+  `;
+  detailContainer.appendChild(mealDiv);
+};
+
+loadMeals("");
---
+++ quotes.js
@@ -0,0 +1,14 @@
+const loadQuote = () =>{
+    fetch('https://api.kanye.rest/')
+    .then(res => res.json())
+    .then(data => displayQuote(data))
+}
+
+const displayQuote = quote =>{
+    const blockQuote =document.getElementById('quote');
+    console.log(quote);
+    blockQuote.innerText=quote.quote;
+
+}
+
+loadQuote();
